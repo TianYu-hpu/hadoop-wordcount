14b2cfb5a0fe039de0149c7ff51b5b31536ddcc7
tianyu <tianyu.hpu@gmail.com>
2017-02-28 23:06:39 +0800

    add HDFS fs operate

--- src/test/java/cn/com/zenmaster/wordcount/AppTest.java
+++ src/test/java/cn/com/zenmaster/wordcount/AppTest.java
@@ -1,38 +1,77 @@
 package cn.com.zenmaster.wordcount;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+	@Test
+	public void testMkdir() {
+		try {
+			// 创建文件系统
+			FileSystem fs = FileSystem.get(new URI("hdfs://主机名或ip地址:8020"), new Configuration());
+			// 创建目录
+			fs.mkdirs(new Path("/upload"));
+			// 关闭资源
+			fs.close();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	@Test
+	public void testUpload() {
+		try {
+			// 创建文件系统
+			FileSystem fs = FileSystem.get(new URI("hdfs://主机名或ip地址:8020"), new Configuration());
+			// 上传文件
+			FSDataOutputStream fsDataOutputStream = fs.create(new Path("/upload/profile"));
+			FileInputStream in = new FileInputStream(new File("/etc/profile"));
+			IOUtils.copyBytes(in, fsDataOutputStream, 2048, true);
+			// 关闭资源
+			fs.close();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void testDel() {
+		try {
+			// 创建文件系统
+			FileSystem fs = FileSystem.get(new URI("hdfs://主机名或ip地址:8020"), new Configuration());
+			fs.delete(new Path("/upload"), true);
+			// 关闭资源
+			fs.close();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
 }
